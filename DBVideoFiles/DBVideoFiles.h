7f10742d443685839ac529eb561e8c176f3a5ebd
mahori <mahori@users.noreply.github.com>
2020-04-17 17:01:45 +0900

    ファイルを追加

---
+++ DBVideoFiles/DBVideoFiles.h
@@ -0,0 +1,32 @@
+﻿
+// DBVideoFiles.h : PROJECT_NAME アプリケーションのメイン ヘッダー ファイルです
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "PCH に対してこのファイルをインクルードする前に 'pch.h' をインクルードしてください"
+#endif
+
+#include "resource.h"		// メイン シンボル
+
+
+// CDBVideoFilesApp:
+// このクラスの実装については、DBVideoFiles.cpp を参照してください
+//
+
+class CDBVideoFilesApp : public CWinApp
+{
+public:
+	CDBVideoFilesApp();
+
+// オーバーライド
+public:
+	virtual BOOL InitInstance();
+
+// 実装
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CDBVideoFilesApp theApp;
